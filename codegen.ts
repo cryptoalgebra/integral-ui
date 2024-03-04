9d4dc009ed2482860117cf8ed659d4f340c2cf68
damnnou <skdamn@outlook.com>
2024-03-04 19:41:58 +0300

    add: farming client to graphql

--- codegen.ts
+++ codegen.ts
@@ -1,22 +1,26 @@
-
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  overwrite: true,
-  schema: [
-    "https://api.thegraph.com/subgraphs/name/iliaazhel/integral-core",
-    "https://api.thegraph.com/subgraphs/name/iliaazhel/goerli-blocks"
-  ],
-  documents: "src/graphql/queries/!(*.d).{ts,tsx}",
-  generates: {
-    "src/graphql/generated/graphql.tsx": {
-      plugins: ['typescript', 'typescript-operations', 'typescript-react-apollo'],
-      config: {
-        withHooks: true,
-        withResultType: true
-      }
-    }
-  }
+    overwrite: true,
+    schema: [
+        'https://api.thegraph.com/subgraphs/name/iliaazhel/integral-core',
+        'https://api.thegraph.com/subgraphs/name/iliaazhel/goerli-blocks',
+        'https://api.thegraph.com/subgraphs/name/iliaazhel/farming-test',
+    ],
+    documents: 'src/graphql/queries/!(*.d).{ts,tsx}',
+    generates: {
+        'src/graphql/generated/graphql.tsx': {
+            plugins: [
+                'typescript',
+                'typescript-operations',
+                'typescript-react-apollo',
+            ],
+            config: {
+                withHooks: true,
+                withResultType: true,
+            },
+        },
+    },
 };
 
 export default config;
