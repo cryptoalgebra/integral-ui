022b4997b133f4ad98399b3cc43acc7637b85097
damnnou <skdamn@outlook.com>
2024-03-20 17:21:31 +0300

    add: avg, max aprs to pools table, apr to active farming

--- src/components/common/Table/poolsColumns.tsx
+++ src/components/common/Table/poolsColumns.tsx
@@ -1,13 +1,13 @@
-import { ColumnDef } from "@tanstack/react-table";
-import { HeaderItem } from "./common";
-import { Address } from "wagmi";
-import { useCurrency } from "@/hooks/common/useCurrency";
-import CurrencyLogo from "../CurrencyLogo";
-import { TokenFieldsFragment } from "@/graphql/generated/graphql";
-import { DynamicFeePluginIcon } from "../PluginIcons";
-import { formatUSD } from "@/utils/common/formatUSD";
-import { usePoolPlugins } from "@/hooks/pools/usePoolPlugins";
-import { Skeleton } from "@/components/ui/skeleton";
+import { ColumnDef } from '@tanstack/react-table';
+import { HeaderItem } from './common';
+import { Address } from 'wagmi';
+import { useCurrency } from '@/hooks/common/useCurrency';
+import CurrencyLogo from '../CurrencyLogo';
+import { TokenFieldsFragment } from '@/graphql/generated/graphql';
+import { DynamicFeePluginIcon } from '../PluginIcons';
+import { formatUSD } from '@/utils/common/formatUSD';
+import { usePoolPlugins } from '@/hooks/pools/usePoolPlugins';
+import { Skeleton } from '@/components/ui/skeleton';
 
 interface Pair {
     token0: TokenFieldsFragment;
@@ -20,67 +20,121 @@ interface Pool {
     fee: number;
     tvlUSD: number;
     volume24USD: number;
-    apr: number;
+    maxApr: number;
+    avgApr: number;
 }
 
 const PoolPair = ({ pair, fee }: Pool) => {
+    const currencyA = useCurrency(pair.token0.id as Address);
+    const currencyB = useCurrency(pair.token1.id as Address);
 
-    const currencyA = useCurrency(pair.token0.id as Address)
-    const currencyB = useCurrency(pair.token1.id as Address)
+    return (
+        <div className="flex items-center gap-4 ml-2">
+            <div className="flex">
+                <CurrencyLogo currency={currencyA} size={30} />
+                <CurrencyLogo
+                    currency={currencyB}
+                    size={30}
+                    className="-ml-2"
+                />
+            </div>
 
-    return <div className="flex items-center gap-4 ml-2">
+            {currencyA && currencyB ? (
+                <div>{`${currencyA?.symbol} - ${currencyB?.symbol}`}</div>
+            ) : (
+                <Skeleton className="h-[20px] w-[90px] bg-card" />
+            )}
 
-        <div className="flex">
-            <CurrencyLogo currency={currencyA} size={30} />
-            <CurrencyLogo currency={currencyB} size={30} className="-ml-2" />
+            <div className="bg-muted-primary text-primary-text rounded-xl px-2 py-1">{`${fee}%`}</div>
         </div>
-
-        {currencyA && currencyB ? <div>{`${currencyA?.symbol} - ${currencyB?.symbol}`}</div> : <Skeleton className="h-[20px] w-[90px] bg-card" /> }
-
-        <div className="bg-muted-primary text-primary-text rounded-xl px-2 py-1">{`${fee}%`}</div>
-
-    </div>
-
-}
+    );
+};
 
 const Plugins = ({ poolId }: { poolId: Address }) => {
+    const { dynamicFeePlugin } = usePoolPlugins(poolId);
 
-    const { dynamicFeePlugin } = usePoolPlugins(poolId)
-
-    return <div className="flex gap-2">
-        {dynamicFeePlugin && <DynamicFeePluginIcon />}
-    </div>
-}
+    return (
+        <div className="flex gap-2">
+            {dynamicFeePlugin && <DynamicFeePluginIcon />}
+        </div>
+    );
+};
 
 export const poolsColumns: ColumnDef<Pool>[] = [
     {
         accessorKey: 'pair',
         header: () => <HeaderItem className="ml-2">Pool</HeaderItem>,
         cell: ({ row }) => <PoolPair {...row.original} />,
-        filterFn: (v, _, value) =>  [
-                v.original.pair.token0.symbol, 
-                v.original.pair.token1.symbol , 
-                v.original.pair.token0.name, 
-                v.original.pair.token1.name
-            ].join(' ').toLowerCase().includes(value)
+        filterFn: (v, _, value) =>
+            [
+                v.original.pair.token0.symbol,
+                v.original.pair.token1.symbol,
+                v.original.pair.token0.name,
+                v.original.pair.token1.name,
+            ]
+                .join(' ')
+                .toLowerCase()
+                .includes(value),
     },
     {
         accessorKey: 'plugins',
-        header: () =>  <HeaderItem>Plugins</HeaderItem>,
+        header: () => <HeaderItem>Plugins</HeaderItem>,
         cell: ({ row }) => <Plugins poolId={row.original.id} />,
     },
     {
         accessorKey: 'tvlUSD',
-        header: ({ column }) => <HeaderItem sort={() => column.toggleSorting(column.getIsSorted() === "asc")} isAsc={column.getIsSorted() === "asc"}>TVL</HeaderItem>,
-        cell: ({ getValue }) => formatUSD.format(getValue() as number)
+        header: ({ column }) => (
+            <HeaderItem
+                sort={() =>
+                    column.toggleSorting(column.getIsSorted() === 'asc')
+                }
+                isAsc={column.getIsSorted() === 'asc'}
+            >
+                TVL
+            </HeaderItem>
+        ),
+        cell: ({ getValue }) => formatUSD.format(getValue() as number),
     },
     {
         accessorKey: 'volume24USD',
-        header: ({ column }) => <HeaderItem sort={() => column.toggleSorting(column.getIsSorted() === "asc")} isAsc={column.getIsSorted() === "asc"}>Volume 24H</HeaderItem>,
-        cell: ({ getValue }) => formatUSD.format(getValue() as number)
+        header: ({ column }) => (
+            <HeaderItem
+                sort={() =>
+                    column.toggleSorting(column.getIsSorted() === 'asc')
+                }
+                isAsc={column.getIsSorted() === 'asc'}
+            >
+                Volume 24H
+            </HeaderItem>
+        ),
+        cell: ({ getValue }) => formatUSD.format(getValue() as number),
     },
     {
-        accessorKey: 'apr',
-        header: ({ column }) => <HeaderItem sort={() => column.toggleSorting(column.getIsSorted() === "asc")} isAsc={column.getIsSorted() === "asc"}>APR</HeaderItem>,
-    }
-]
+        accessorKey: 'maxApr',
+        header: ({ column }) => (
+            <HeaderItem
+                sort={() =>
+                    column.toggleSorting(column.getIsSorted() === 'asc')
+                }
+                isAsc={column.getIsSorted() === 'asc'}
+            >
+                Max. APR
+            </HeaderItem>
+        ),
+        cell: ({ getValue }) => `${getValue()} %`,
+    },
+    {
+        accessorKey: 'avgApr',
+        header: ({ column }) => (
+            <HeaderItem
+                sort={() =>
+                    column.toggleSorting(column.getIsSorted() === 'asc')
+                }
+                isAsc={column.getIsSorted() === 'asc'}
+            >
+                Avg. APR
+            </HeaderItem>
+        ),
+        cell: ({ getValue }) => `${getValue()} %`,
+    },
+];
--- src/components/pools/PoolsList/index.tsx
+++ src/components/pools/PoolsList/index.tsx
@@ -1,35 +1,48 @@
-import DataTable from "@/components/common/Table/dataTable"
-import { poolsColumns } from "@/components/common/Table/poolsColumns"
-import { usePoolsListQuery } from "@/graphql/generated/graphql"
-import { useMemo } from "react"
-import { Address } from "viem"
+import DataTable from '@/components/common/Table/dataTable';
+import { poolsColumns } from '@/components/common/Table/poolsColumns';
+import { usePoolsListQuery } from '@/graphql/generated/graphql';
+import { useMemo } from 'react';
+import { Address } from 'viem';
+import { POOL_AVG_APR_API, POOL_MAX_APR_API, fetcher } from '@/constants/api';
+import useSWR from 'swr';
 
 const PoolsList = () => {
+    const { data: pools, loading } = usePoolsListQuery();
 
-    const { data: pools, loading } = usePoolsListQuery()
+    const { data: poolsMaxApr } = useSWR(POOL_MAX_APR_API, fetcher);
+    const { data: poolsAvgApr } = useSWR(POOL_AVG_APR_API, fetcher);
 
     const formattedPools = useMemo(() => {
+        if (!pools?.pools || !poolsMaxApr || !poolsAvgApr) return [];
 
-        if (!pools?.pools) return []
+        return pools.pools.map(
+            ({ id, token0, token1, fee, totalValueLockedUSD, volumeUSD }) => ({
+                id: id as Address,
+                pair: {
+                    token0,
+                    token1,
+                },
+                fee: Number(fee) / 10_000,
+                tvlUSD: Number(totalValueLockedUSD),
+                volume24USD: Number(volumeUSD),
+                maxApr: poolsMaxApr[id].toFixed(2),
+                avgApr: poolsAvgApr[id].toFixed(2),
+            })
+        );
+    }, [pools, poolsMaxApr, poolsAvgApr]);
 
-        return pools.pools.map(({ id, token0, token1, fee, totalValueLockedUSD, volumeUSD }) => ({
-            id: id as Address,
-            pair: {
-                token0,
-                token1
-            },
-            fee: Number(fee) / 10_000,
-            tvlUSD: Number(totalValueLockedUSD),
-            volume24USD: Number(volumeUSD),
-            apr: 0
-        }))
+    return (
+        <div className="flex flex-col gap-4">
+            <DataTable
+                columns={poolsColumns}
+                data={formattedPools}
+                defaultSortingID={'tvlUSD'}
+                link={'pool'}
+                showPagination={false}
+                loading={loading}
+            />
+        </div>
+    );
+};
 
-    }, [pools])
-
-    return <div className="flex flex-col gap-4">
-        <DataTable columns={poolsColumns} data={formattedPools} defaultSortingID={'tvlUSD'} link={'pool'} showPagination={false} loading={loading}  />
-    </div>
-
-}
-
-export default PoolsList
+export default PoolsList;
