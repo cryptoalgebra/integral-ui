a9a59bd330b29509b5f3d44f71d36facd02ea6db
damnnou <skdamn@outlook.com>
2024-03-22 17:52:17 +0300

    fix: add poolsVolumeData query, fix 24h volume in pools table

--- src/components/pools/PoolsList/index.tsx
+++ src/components/pools/PoolsList/index.tsx
@@ -1,6 +1,9 @@
 import DataTable from '@/components/common/Table/dataTable';
 import { poolsColumns } from '@/components/common/Table/poolsColumns';
-import { usePoolsListQuery } from '@/graphql/generated/graphql';
+import {
+    usePoolsListQuery,
+    usePoolsVolumeDataQuery,
+} from '@/graphql/generated/graphql';
 import { useMemo } from 'react';
 import { Address } from 'viem';
 import { POOL_AVG_APR_API, POOL_MAX_APR_API, fetcher } from '@/constants/api';
@@ -9,27 +12,40 @@ import useSWR from 'swr';
 const PoolsList = () => {
     const { data: pools, loading } = usePoolsListQuery();
 
+    const { data: poolsVolume } = usePoolsVolumeDataQuery();
+
     const { data: poolsMaxApr } = useSWR(POOL_MAX_APR_API, fetcher);
     const { data: poolsAvgApr } = useSWR(POOL_AVG_APR_API, fetcher);
 
     const formattedPools = useMemo(() => {
-        if (!pools?.pools || !poolsMaxApr || !poolsAvgApr) return [];
+        if (
+            !pools?.pools ||
+            !poolsMaxApr ||
+            !poolsAvgApr ||
+            !poolsVolume?.poolDayDatas
+        )
+            return [];
 
         return pools.pools.map(
-            ({ id, token0, token1, fee, totalValueLockedUSD, volumeUSD }) => ({
-                id: id as Address,
-                pair: {
-                    token0,
-                    token1,
-                },
-                fee: Number(fee) / 10_000,
-                tvlUSD: Number(totalValueLockedUSD),
-                volume24USD: Number(volumeUSD),
-                maxApr: poolsMaxApr[id] ? poolsMaxApr[id].toFixed(2) : 0,
-                avgApr: poolsAvgApr[id] ? poolsAvgApr[id].toFixed(2) : 0,
-            })
+            ({ id, token0, token1, fee, totalValueLockedUSD }) => {
+                const currentPool = poolsVolume.poolDayDatas.find(
+                    (currPool) => currPool.pool.id === id
+                );
+                return {
+                    id: id as Address,
+                    pair: {
+                        token0,
+                        token1,
+                    },
+                    fee: Number(fee) / 10_000,
+                    tvlUSD: Number(totalValueLockedUSD),
+                    volume24USD: Number(currentPool?.volumeUSD),
+                    maxApr: poolsMaxApr[id] ? poolsMaxApr[id].toFixed(2) : 0,
+                    avgApr: poolsAvgApr[id] ? poolsAvgApr[id].toFixed(2) : 0,
+                };
+            }
         );
-    }, [pools, poolsMaxApr, poolsAvgApr]);
+    }, [pools, poolsMaxApr, poolsAvgApr, poolsVolume]);
 
     return (
         <div className="flex flex-col gap-4">
