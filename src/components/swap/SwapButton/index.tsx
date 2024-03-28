a47220e29c442897790d1e18155807cf595be02c
damnnou <skdamn@outlook.com>
2024-03-28 19:09:04 +0300

    add: rates change status before swap

--- src/components/swap/SwapButton/index.tsx
+++ src/components/swap/SwapButton/index.tsx
@@ -1,33 +1,55 @@
-import Loader from "@/components/common/Loader";
-import { Button } from "@/components/ui/button";
-import { DEFAULT_CHAIN_ID, DEFAULT_CHAIN_NAME } from "@/constants/default-chain-id";
-import { useApproveCallbackFromTrade } from "@/hooks/common/useApprove";
-import { useUSDCValue } from "@/hooks/common/useUSDCValue";
-import { useQuotesResults } from "@/hooks/swap/useQuotesResults";
-import { useSwapCallback } from "@/hooks/swap/useSwapCallback";
-import useWrapCallback, { WrapType } from "@/hooks/swap/useWrapCallback";
-import { useDerivedSwapInfo, useSwapState } from "@/state/swapStore";
-import { ApprovalState } from "@/types/approve-state";
-import { SwapField } from "@/types/swap-field";
-import { TradeState } from "@/types/trade-state";
-import { computeFiatValuePriceImpact } from "@/utils/swap/computePriceImpact";
-import { warningSeverity } from "@/utils/swap/prices";
-import { useWeb3Modal, useWeb3ModalState } from "@web3modal/wagmi/react";
-import { useCallback, useEffect, useMemo } from "react";
-import { useAccount } from "wagmi";
+import Loader from '@/components/common/Loader';
+import { Button } from '@/components/ui/button';
+import {
+    DEFAULT_CHAIN_ID,
+    DEFAULT_CHAIN_NAME,
+} from '@/constants/default-chain-id';
+import { useApproveCallbackFromTrade } from '@/hooks/common/useApprove';
+import { useUSDCValue } from '@/hooks/common/useUSDCValue';
+import { useQuotesResults } from '@/hooks/swap/useQuotesResults';
+import { useSwapCallback } from '@/hooks/swap/useSwapCallback';
+import useWrapCallback, { WrapType } from '@/hooks/swap/useWrapCallback';
+import { useDerivedSwapInfo, useSwapState } from '@/state/swapStore';
+import { ApprovalState } from '@/types/approve-state';
+import { SwapField } from '@/types/swap-field';
+import { QuoteResult } from '@/types/swap-quote';
+import { TradeState } from '@/types/trade-state';
+import { computeFiatValuePriceImpact } from '@/utils/swap/computePriceImpact';
+import { warningSeverity } from '@/utils/swap/prices';
+import { Currency, CurrencyAmount } from '@cryptoalgebra/integral-sdk';
+import { useWeb3Modal, useWeb3ModalState } from '@web3modal/wagmi/react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useAccount } from 'wagmi';
 
 const SwapButton = () => {
+    const { open } = useWeb3Modal();
 
-    const { open } = useWeb3Modal()
+    const [isRatesChanged, setIsRatesChanged] = useState<boolean>(false);
 
-    const { selectedNetworkId } = useWeb3ModalState()
+    const { selectedNetworkId } = useWeb3ModalState();
 
-    const { address: account } = useAccount()
+    const { address: account } = useAccount();
 
     const { independentField, typedValue } = useSwapState();
-    const { tradeState, toggledTrade: trade, allowedSlippage, parsedAmount, currencies, inputError: swapInputError } = useDerivedSwapInfo();
+    const {
+        tradeState,
+        toggledTrade: trade,
+        allowedSlippage,
+        parsedAmount,
+        currencies,
+        inputError: swapInputError,
+    } = useDerivedSwapInfo();
 
-    const { wrapType, execute: onWrap, loading: isWrapLoading, inputError: wrapInputError } = useWrapCallback(currencies[SwapField.INPUT], currencies[SwapField.OUTPUT], typedValue);
+    const {
+        wrapType,
+        execute: onWrap,
+        loading: isWrapLoading,
+        inputError: wrapInputError,
+    } = useWrapCallback(
+        currencies[SwapField.INPUT],
+        currencies[SwapField.OUTPUT],
+        typedValue
+    );
 
     const showWrap = wrapType !== WrapType.NOT_APPLICABLE;
 
@@ -43,33 +65,60 @@ const SwapButton = () => {
 
     const parsedAmounts = useMemo(
         () => ({
-            [SwapField.INPUT]: independentField === SwapField.INPUT ? parsedAmount : trade?.inputAmount,
-            [SwapField.OUTPUT]: independentField === SwapField.OUTPUT ? parsedAmount : trade?.outputAmount,
+            [SwapField.INPUT]:
+                independentField === SwapField.INPUT
+                    ? parsedAmount
+                    : trade?.inputAmount,
+            [SwapField.OUTPUT]:
+                independentField === SwapField.OUTPUT
+                    ? parsedAmount
+                    : trade?.outputAmount,
         }),
         [independentField, parsedAmount, trade]
     );
 
-    const userHasSpecifiedInputOutput = Boolean(currencies[SwapField.INPUT] && currencies[SwapField.OUTPUT] && parsedAmounts[independentField]?.greaterThan('0'));
+    const userHasSpecifiedInputOutput = Boolean(
+        currencies[SwapField.INPUT] &&
+            currencies[SwapField.OUTPUT] &&
+            parsedAmounts[independentField]?.greaterThan('0')
+    );
 
-    const isExpertMode = true
+    const isExpertMode = true;
     const routeNotFound = !trade?.route;
     const isLoadingRoute = TradeState.LOADING === tradeState.state;
 
-    const { price: fiatValueInputPrice } = useUSDCValue(parsedAmounts[SwapField.INPUT]);
-    const { price: fiatValueOutputPrice } = useUSDCValue(parsedAmounts[SwapField.OUTPUT]);
+    const { price: fiatValueInputPrice } = useUSDCValue(
+        parsedAmounts[SwapField.INPUT]
+    );
+    const { price: fiatValueOutputPrice } = useUSDCValue(
+        parsedAmounts[SwapField.OUTPUT]
+    );
 
-    const priceImpact = computeFiatValuePriceImpact(fiatValueInputPrice, fiatValueOutputPrice);
+    const priceImpact = computeFiatValuePriceImpact(
+        fiatValueInputPrice,
+        fiatValueOutputPrice
+    );
 
-    const { approvalState, approvalCallback } = useApproveCallbackFromTrade(trade, allowedSlippage);
+    const { approvalState, approvalCallback } = useApproveCallbackFromTrade(
+        trade,
+        allowedSlippage
+    );
 
     const priceImpactSeverity = useMemo(() => {
         const executionPriceImpact = trade?.priceImpact;
-        return warningSeverity(executionPriceImpact && priceImpact ? (executionPriceImpact.greaterThan(priceImpact) ? executionPriceImpact : priceImpact) : executionPriceImpact ?? priceImpact);
+        return warningSeverity(
+            executionPriceImpact && priceImpact
+                ? executionPriceImpact.greaterThan(priceImpact)
+                    ? executionPriceImpact
+                    : priceImpact
+                : executionPriceImpact ?? priceImpact
+        );
     }, [priceImpact, trade]);
 
     const showApproveFlow =
         !swapInputError &&
-        (approvalState === ApprovalState.NOT_APPROVED || approvalState === ApprovalState.PENDING) &&
+        (approvalState === ApprovalState.NOT_APPROVED ||
+            approvalState === ApprovalState.PENDING) &&
         !(priceImpactSeverity > 3 && !isExpertMode);
 
     const { refetch: refetchQuotesA } = useQuotesResults({
@@ -78,41 +127,80 @@ const SwapButton = () => {
         currencyOut: currencies[SwapField.OUTPUT],
     });
 
-    const { refetch: refetchQuotesB } = useQuotesResults({
-        exactInput: true,
-        amountOut: parsedAmountB,
-        currencyIn: currencies[SwapField.INPUT],
-    });
-    
-    const { callback: swapCallback, error: swapCallbackError, isLoading: isSwapLoading, isSuccess: isSwapSuccess } = useSwapCallback(trade, allowedSlippage);
+    const {
+        callback: swapCallback,
+        error: swapCallbackError,
+        isLoading: isSwapLoading,
+        isSuccess: isSwapSuccess,
+    } = useSwapCallback(trade, allowedSlippage);
 
     const handleSwap = useCallback(async () => {
-        if (!swapCallback) return;
-        await refetchQuotesA();
-        await refetchQuotesB();
+        if (!swapCallback || !parsedAmountB || !currencies) return;
+        try {
+            const { data } = await refetchQuotesA();
+            const quoteResult = data?.[0]?.result as QuoteResult;
 
-        swapCallback().catch((error) => new Error(`Swap Failed ${error}`));
-    }, [swapCallback, refetchQuotesA, refetchQuotesB]);
+            const quoteAmountB = quoteResult[0];
+
+            const parsedQuoteAmountB = CurrencyAmount.fromRawAmount(
+                currencies[SwapField.OUTPUT] as Currency,
+                quoteAmountB.toString()
+            );
+
+            if (parsedQuoteAmountB.toExact() !== parsedAmountB.toExact()) {
+                setIsRatesChanged(true);
+                return;
+            } else {
+                setIsRatesChanged(false);
+            }
+
+            await swapCallback();
+        } catch (error) {
+            return new Error(`Swap Failed ${error}`);
+        }
+    }, [swapCallback, refetchQuotesA, currencies, parsedAmountB]);
 
     useEffect(() => {
         if (!isSwapSuccess) return;
         refetchQuotesA();
-        refetchQuotesB();
-    }, [isSwapSuccess]);
+    }, [isSwapSuccess, refetchQuotesA]);
+
+    useEffect(() => {
+        if (!isRatesChanged) return;
+        setIsRatesChanged(false);
+    }, [typedValue]);
 
     const isValid = !swapInputError;
 
     const priceImpactTooHigh = priceImpactSeverity > 3 && !isExpertMode;
 
-    const isWrongChain = selectedNetworkId !== DEFAULT_CHAIN_ID
+    const isWrongChain = selectedNetworkId !== DEFAULT_CHAIN_ID;
 
-    if (!account) return <Button onClick={() => open()}>Connect Wallet</Button>
+    if (!account) return <Button onClick={() => open()}>Connect Wallet</Button>;
 
-    if (isWrongChain) return <Button variant={'destructive'} onClick={() => open({view: 'Networks'})}>{`Connect to ${DEFAULT_CHAIN_NAME}`}</Button>
+    if (isWrongChain)
+        return (
+            <Button
+                variant={'destructive'}
+                onClick={() => open({ view: 'Networks' })}
+            >{`Connect to ${DEFAULT_CHAIN_NAME}`}</Button>
+        );
 
-    if (showWrap && wrapInputError) return <Button disabled>{wrapInputError}</Button>
+    if (showWrap && wrapInputError)
+        return <Button disabled>{wrapInputError}</Button>;
 
-    if (showWrap) return <Button onClick={() => onWrap && onWrap()}>{isWrapLoading ? <Loader/> : wrapType === WrapType.WRAP ? 'Wrap' : 'Unwrap'}</Button>
+    if (showWrap)
+        return (
+            <Button onClick={() => onWrap && onWrap()}>
+                {isWrapLoading ? (
+                    <Loader />
+                ) : wrapType === WrapType.WRAP ? (
+                    'Wrap'
+                ) : (
+                    'Unwrap'
+                )}
+            </Button>
+        );
 
     if (routeNotFound && userHasSpecifiedInputOutput)
         return (
@@ -126,20 +214,51 @@ const SwapButton = () => {
         );
 
     if (showApproveFlow)
-        return (<Button disabled={approvalState !== ApprovalState.NOT_APPROVED} onClick={() => approvalCallback && approvalCallback()}>
-            {approvalState === ApprovalState.PENDING ? <Loader /> : approvalState === ApprovalState.APPROVED ? 'Approved' : `Approve ${currencies[SwapField.INPUT]?.symbol}`}
-        </Button>
+        return (
+            <Button
+                disabled={approvalState !== ApprovalState.NOT_APPROVED}
+                onClick={() => approvalCallback && approvalCallback()}
+            >
+                {approvalState === ApprovalState.PENDING ? (
+                    <Loader />
+                ) : approvalState === ApprovalState.APPROVED ? (
+                    'Approved'
+                ) : (
+                    `Approve ${currencies[SwapField.INPUT]?.symbol}`
+                )}
+            </Button>
         );
 
     return (
-        <Button
-            onClick={() => handleSwap()}
-            disabled={!isValid || priceImpactTooHigh || !!swapCallbackError || isSwapLoading}
-        >
-            {isSwapLoading ? <Loader /> : swapInputError ? swapInputError : priceImpactTooHigh ? 'Price Impact Too High' : priceImpactSeverity > 2 ? 'Swap Anyway' : 'Swap'}
-        </Button>
+        <>
+            <Button
+                onClick={() => handleSwap()}
+                disabled={
+                    !isValid ||
+                    priceImpactTooHigh ||
+                    !!swapCallbackError ||
+                    isSwapLoading
+                }
+            >
+                {isSwapLoading ? (
+                    <Loader />
+                ) : swapInputError ? (
+                    swapInputError
+                ) : priceImpactTooHigh ? (
+                    'Price Impact Too High'
+                ) : priceImpactSeverity > 2 ? (
+                    'Swap Anyway'
+                ) : (
+                    'Swap'
+                )}
+            </Button>
+            {isRatesChanged && (
+                <span className="text-neutral-400 text-sm my-1">
+                    Rates have been changed
+                </span>
+            )}
+        </>
     );
-
-}
+};
 
 export default SwapButton;
