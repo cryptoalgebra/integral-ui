758c44667d1c252261fb4766ac609e976640fa71
Kirill S <skdamn@outlook.com>
2025-04-25 17:35:10 +0300

    chore: ban fake ai pool

--- src/components/common/CurrencyLogo/index.tsx
+++ src/components/common/CurrencyLogo/index.tsx
@@ -27,6 +27,10 @@ export const specialTokens: { [key: Address]: { symbol: string; logo: string } }
         symbol: "USDT",
         logo: USDTLogo,
     },
+    ["0xfde4c96c8593536e31f229ea8f37b2ada2699bb2"]: {
+        symbol: "USDT",
+        logo: USDTLogo,
+    },
     ["0x833589fcd6edb6e08f4c7c32d4f71b54bda02913"]: {
         symbol: "USDC",
         logo: USDCLogo,
--- src/constants/pools.ts
+++ src/constants/pools.ts
@@ -1,1 +1,3 @@
-export const blacklistedPools = ["0x60680Db4244a0Ed3589ccd48D878d1AbfcC88D54"].map((address) => address.toLowerCase());
+export const blacklistedPools = ["0x60680Db4244a0Ed3589ccd48D878d1AbfcC88D54", "0x7456ae73e1325766af64b8c0b5c4ec2f12e5f948"].map(
+    (address) => address.toLowerCase()
+);
