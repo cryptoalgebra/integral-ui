e9a83b581f8a94faa10b9f8e8dfa7b41c0abdf65
lilchizh <xlilchizhx@gmail.com>
2025-11-11 16:31:41 +0300

    (fix) Fix Wrap/Unwrap

--- src/state/swapStore.ts
+++ src/state/swapStore.ts
@@ -188,8 +188,8 @@ export function useDerivedSwapInfo(): IDerivedSwapInfo {
         wasInverted,
     } = useSwapState();
 
-    const inputCurrency = useCurrency(inputCurrencyId);
-    const outputCurrency = useCurrency(outputCurrencyId);
+    const inputCurrency = useCurrency(inputCurrencyId, false);
+    const outputCurrency = useCurrency(outputCurrencyId, false);
 
     const isExactIn: boolean = independentField === SwapField.INPUT;
 
