06edea3940b8774b46d18a24575005698bc1f21a
damnnou <skdamn@outlook.com>
2025-11-20 01:10:22 +0300

    config: bump omega router, add steakEURA and re7 eUSD

--- config/tokens.ts
+++ config/tokens.ts
@@ -4,6 +4,8 @@ export const TOKENS = {
     [ChainId.Base]: {
         USDC: new Token(ChainId.Base, "0x833589fcd6edb6e08f4c7c32d4f71b54bda02913", 6, "USDC", "USDC"),
         USDT: new Token(ChainId.Base, "0xfde4C96c8593536E31F229EA8f37b2ADa2699bb2", 6, "USDT", "USDT"),
+        agEUR: new Token(ChainId.Base, "0xA61BeB4A3d02decb01039e378237032B351125B4", 18, "agEUR", "agEUR"),
+        eUSD: new Token(ChainId.Base, "0xCfA3Ef56d303AE4fAabA0592388F19d7C3399FB4", 18, "eUSD", "Electronic Dollar"),
     },
     [ChainId.BaseSepolia]: {
         USDC: new Token(ChainId.BaseSepolia, "0xAbAc6f23fdf1313FC2E9C9244f666157CcD32990", 6, "USDC", "USDC"),
@@ -28,6 +30,22 @@ export const BOOSTED_TOKENS = {
             "mwETH",
             "Moonwell Flagship ETH",
             WNATIVE[ChainId.Base]
+        ),
+        steakEURA: new BoostedToken(
+            ChainId.Base,
+            "0xBEeFA28D5e56d41D35df760AB53B94D9FfD7051F",
+            18,
+            "steakEURA",
+            "Steakhouse EURA",
+            TOKENS[ChainId.Base].agEUR
+        ),
+        ["Re7 eUSD"]: new BoostedToken(
+            ChainId.Base,
+            "0xc9C474C6Aa0E930ED42fD1f40be5b1d6A5eAD645",
+            18,
+            "Re7 eUSD",
+            "Re7 eUSD",
+            TOKENS[ChainId.Base].eUSD
         ),
     },
 };
