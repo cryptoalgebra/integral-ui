28bb8f5f014d58a216b6a41d1e5cbd1270fa5cda
damnnou <skdamn@outlook.com>
2026-01-29 12:16:12 +0300

    config: update api urls

--- config/apr-urls.ts
+++ config/apr-urls.ts
@@ -1,7 +1,7 @@
 export const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
-export const ETERNAL_FARMINGS_API = "https://api.algebra.finance/api/APR/eternalFarmings/?network=henesys";
+export const ETERNAL_FARMINGS_API = "https://maple-apr.algebra.finance/api/eternal-farmings/apr?network=henesys";
 
-export const POOL_MAX_APR_API = "https://api.algebra.finance/api/APR/pools/max?network=henesys";
+export const POOL_MAX_APR_API = "https://maple-apr.algebra.finance/api/pools/max-apr?network=henesys";
 
-export const POOL_AVG_APR_API = "https://api.algebra.finance/api/APR/pools/?network=henesys";
+export const POOL_AVG_APR_API = "https://maple-apr.algebra.finance/api/pools/apr?network=henesys";
