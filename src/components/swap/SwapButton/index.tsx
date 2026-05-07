8e5e16b4d3fa3e35870d236bbc2011ceeaf45b43
damnnou <skdamn@outlook.com>
2026-05-07 17:46:09 +0300

    chore: price impact severity

--- src/components/swap/SwapButton/index.tsx
+++ src/components/swap/SwapButton/index.tsx
@@ -206,7 +206,7 @@ const SwapButton = ({ derivedSwap }: { derivedSwap: IDerivedSwapInfo }) => {
 
     const isValid = !swapInputError && !activeSwapError;
 
-    const hasLargePriceDifference = priceImpactSeverity > 3;
+    const hasLargePriceDifference = priceImpactSeverity > 2;
 
     const largePriceDifferencePercent = useMemo(() => {
         if (!priceImpact) return "0.00";
