9f0b178a52946b92211810ed4f83ee5ab09f5885
damnnou <skdamn@outlook.com>
2026-08-21 16:08:44 +0300

    config: add nav custom pool deployer

--- config/custom-pool-deployer.ts
+++ config/custom-pool-deployer.ts
@@ -1,16 +1,20 @@
 import { ADDRESS_ZERO, ChainId } from "@cryptoalgebra/integral-sdk";
 import { Address } from "viem";
 
-export type PoolDeployerType = "BASE_DYNAMIC";
+export type PoolDeployerType = "BASE_DYNAMIC" | "NAV";
 
 export const CUSTOM_POOL_DEPLOYER_ADDRESSES: Record<PoolDeployerType, Record<number, Address | null>> = {
     BASE_DYNAMIC: {
         [ChainId.Base]: ADDRESS_ZERO,
     },
+    NAV: {
+        [ChainId.Base]: "0xD0cffd4764c2BC08D79C7d54B2A95902b05c51F9",
+    },
 } as const;
 
 export const CUSTOM_POOL_DEPLOYER_TITLES: Record<PoolDeployerType, string> = {
     BASE_DYNAMIC: "Dynamic",
+    NAV: "NAV",
 } as const;
 
 export const customPoolDeployerTitleByAddress: Record<Address, string> = Object.fromEntries(
